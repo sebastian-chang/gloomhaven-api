9f2018add3a1e46047b6a3da79226efdfb6d53c6
Sebastian <sebastian.chang23@gmail.com>
2021-08-26 22:42:56 -0500

    Added env file

--- gloomhaven/settings.py
+++ gloomhaven/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8pp()ara@!2*$j5fl_y*$d2u4*mpr%7gr5g&0te(wdo&sde71t'
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
